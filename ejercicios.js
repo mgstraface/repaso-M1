1701781fad68c11404bd1d41ce4d1f40d845dafd
jmella22 <ing.jose.mella@gmail.com>
2022-05-10 15:01:03 -0400

    first commit

---
+++ ejercicios.js
@@ -0,0 +1,58 @@
+// ------------------EJERCICIO I-----------------------------//
+/**
+ * Escribe una function llamada BinaryToDecimal que acepte string numerico
+ * y una base.
+ * La funcion deberia retornar un numero entero.
+ * La idea es tomar un numero binario y "pasarlo" a un numero entero.
+ * La formula para convertir un numero binario en decimal es: N * B ^ I
+ * Donde N es el numero B es la base e I es el indice.
+ * Digamos que tenemos el binario "101" la formulara seria asi:
+ * 1 * 2 ^ 2  +  0 * 2 ^ 1  +  1 * 2 ^ 0
+ * @param {Array} arr1 - esto es un array.
+ * @param {Array} arr1 - esto es un array.
+ * @returns {boolean} - deberia retornar true o false.
+ */
+
+function BinaryToDecimal(binary, base) {}
+
+// ------------------EJERCICIO II----------------------------//
+
+/**
+ * Escribe una function llamada same que acepte dos arrays.
+ * La funcion deberia retornar true si cada valor del primer array es
+ * igual al cuadrado de valor del segundo array.
+ * La frecuencia debe ser la misma.
+ * @param {Array} arr1 - esto es un array.
+ * @param {Array} arr1 - esto es un array.
+ * @returns {boolean} - deberia retornar true o false.
+ */
+
+function same(arr1, arr2) {}
+
+// ------------------EJERCICIO III---------------------------//
+/**
+ * Dado dos strings, escribe una function para determinar si el
+ * segundo string es un anagrama de el primero.
+ * Deberia retornar true si cada letra en el primer string se encuentra
+ * en el segundo string.
+ * Un anagrama consiste en crear una palabra a partir de la
+ * reordenación de las letras de otra palabra.
+ * Como cinema y iceman.
+ * @param {string} first - esto es un string.
+ * @param {string} second - esto es un string.
+ * @returns {boolean} - deberia retornar true o false.
+ */
+
+function validAnagram(first, second) {}
+
+// ------------------EJERCICIO IV---------------------------//
+/**
+ * Escribe una function llamada sumZero que acepte un array de numeros
+ * enteros ordenado. La function deberia encontrar el primer para de
+ * numeros que sumados den 0. Retorna un array que incluya los dos
+ * numeros encontrados o undefined si el par no existe..
+ * @param {Array} arr1 - esto es un array.
+ * @returns {Array || undefined} - deberia retornar un array o undefined.
+ */
+
+function sumZero(arr) {}
